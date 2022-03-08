bf80abe8563ee4144bef399da596814b6415e7bd
maurifcarmo <mauri_vrb@hotmail.com>
2022-03-08 09:06:10 -0300

    Improvement on script readability
    Move some code blocks to functions
    Remove unused interactive variables loading
    Add some comments to funtions

--- vault-deploy/postgres_secret_engine_setup/setup.sh
+++ vault-deploy/postgres_secret_engine_setup/setup.sh
@@ -1,18 +1,58 @@
 #!/bin/bash
 
-DATABASE_NAME=""
-DATABASE_HOST=""
-PASSWORD=""
-USER_NAME=""
-PROJECT=""
-INSTANCE=""
+# Check if Vault server is reacheable
+check_vault_server(){
+    echo "Checking Vault connectivity..."
+    vault status > /dev/null
+
+    if [ $? != 0 ];
+    then 
+        printf "\nFailed when connecting to Vault server. Check if your Vault CLI is properly setup."
+        exit 1;
+    else
+        printf "Vault Server up and running.\n\n"
+    fi
+}
+
+# Print variable loaded from .env file
+print_env(){
+    echo "GCP Project: "$PROJECT
+    echo "DB Instance: "$INSTANCE
+    echo "Database Name: "$DATABASE_NAME
+    echo "Database Host: "$DATABASE_HOST
+    echo "Database User: "$USER_NAME
+    echo "Database Pass: ********"
+    echo -n "Roles to create: "
+    for role in ${ROLES[@]}
+    do
+        echo -n "$role "
+    done
+    echo ""
+    echo "Vault Server: "$VAULT_ADDR
+    echo "Vault Cluster Context: "$VAULT_CLUSTER_CONTEXT
+    echo "Vault Deploy Namespace: "$VAULT_NAMESPACE
+    echo "Secret Name: "$SECRET_NAME
+}
+
+# Ask if user wants to procceed after check loaded envs
+ask_user(){
+    printf "\nAre you sure you want to procceed? (yes / no): "
+    read ANSWER
+
+    if [ "$ANSWER" != "yes" ] && [ "$ANSWER" != "y" ];
+    then
+        echo "Aborted by user!"
+        exit 1;
+    fi
+}
 
 # Enable the database secrets engine if it is not already enabled:
 enable_postgres_secret_engine(){
     printf "\nEnabling Vault secret database\n"
-    vault secrets enable database
+    vault secrets enable database 
 }
 
+# TODO: Uncomment connnection url 
 # Config access to target database
 config_vault_database(){
     printf "\nWriting database config to Vault\n"  
@@ -21,13 +61,11 @@ config_vault_database(){
         plugin_name=postgresql-database-plugin \
         allowed_roles=* \
         connection_url="postgresql://{{username}}:{{password}}@${DATABASE_HOST}:5432/${DATABASE_NAME}?sslmode=verify-ca&sslrootcert=/tmp/${SECRET_NAME}/server-ca.pem&sslcert=/tmp/${SECRET_NAME}/client-cert.pem&sslkey=/tmp/${SECRET_NAME}/client-key.pem" \
+        # connection_url="postgresql://{{username}}:{{password}}@${DATABASE_HOST}:5432/${DATABASE_NAME}?sslmode=verify-ca&sslrootcert=/pgsql/${SECRET_NAME}/server-ca.pem&sslcert=/pgsql/${SECRET_NAME}/client-cert.pem&sslkey=/pgsql/${SECRET_NAME}/client-key.pem" \
         username=$USER_NAME \
         password=$PASSWORD
-    
 }
 
-# Configure Vault with the proper plugin and connection information:
-#              GCP   INTANCE/VM
 create_roles(){
 
     for role in ${ROLES[@]}
@@ -54,6 +92,7 @@ EOF
     done
 }
 
+# Insert a secret on K8s containing postgres ca, certificate and key
 insert_secrets_on_k8s(){
     printf "\nRemoving secret (if exists)\n"
     kubectl delete secret $SECRET_NAME \
@@ -69,6 +108,7 @@ insert_secrets_on_k8s(){
         --from-file=server-ca.pem=server-ca.pem
 }
 
+# Patch vault statefulset due to mount the new secret
 patch_vault_statefulset(){
 
     printf "\nCreating patch from template file\n"
@@ -81,6 +121,7 @@ patch_vault_statefulset(){
         --patch-file tmp/vault-patch.yaml
 }
 
+# Restart pods to apply patch
 restart_pods(){
     printf "\nRestarting Vault pods\n"
     kubectl delete pod vault-0 \
@@ -100,6 +141,7 @@ restart_pods(){
         --namespace $VAULT_NAMESPACE
 }
 
+# Print commands to get a credentials from the recent creted roles
 print_results(){
     printf "\nYou can, now, get secrets with the following commands:\n"
 
@@ -110,90 +152,42 @@ print_results(){
 }
 
 # Check if .env exists
-if [ -f .env ];
-then
-    set -a
-    source .env;
-    set +a
-else
-    printf ".env file not found\n\n";
-fi
-
-echo "Checking Vault connectivity..."
-vault status > /dev/null
-
-if [ $? != 0 ];
-then 
-    printf "\nFailed when connecting to Vault server. Check if your Vault CLI is properly setup."
-    exit 1;
-else
-    printf "Vault Server up and running.\n\n"
-fi
-
-if [ ! -n "$DATABASE_NAME" ] || 
-   [ ! -n "$DATABASE_HOST" ] ||
-   [ ! -n "$PASSWORD" ] ||
-   [ ! -n "$USER_NAME" ] ||
-   [ ! -n "$PROJECT" ] ||
-   [ ! -n "$INSTANCE" ]
-then
-    echo -n "GCP Project: "
-    read PROJECT
-    
-    echo -n "Instance: "
-    read INSTANCE
-
-    echo -n "Enter Database name: "
-    read DATABASE_NAME
-
-    echo -n "Enter Database host (IP or dns): "
-    read DATABASE_HOST
-
-    echo -n "Enter user name to connect to database: "
-    read USER_NAME
-
-    echo -n "Enter user password to connect to database: "
-    read PASSWORD
-
-    clear
-else
-    printf "Variables loaded from .env file:\n\n";
-fi
-
-echo "Vault Server: "$VAULT_ADDR
-echo "GCP Project: "$PROJECT
-echo "DB Instance: "$INSTANCE
-echo "Database Name: "$DATABASE_NAME
-echo "Database Host: "$DATABASE_HOST
-echo "Database User: "$USER_NAME
-echo "Database Pass: ********"
-
-printf "\nAre you sure you want to procceed? (yes / no): "
-read ANSWER
-
-if [ "$ANSWER" != "yes" ] && [ "$ANSWER" != "y" ];
-then
-    echo "Aborted by user!"
-    exit 1;
-fi
+load_env_file(){
+    if [ -f .env ];
+    then
+        set -a
+        source .env;
+        set +a
+    else
+        printf ".env file not found\n\n";
+    fi
+}
 
 # Configure Vault with the proper plugin and connection information:
 #              GCP   INTANCE/VM
 DATABASE_URI=$PROJECT-$INSTANCE-$DATABASE_NAME
 
 # MAIN
+
+# Initialization
+load_env_file
+check_vault_server
+print_env
+ask_user
+
+# Enable Postgres Secret Engine
 enable_postgres_secret_engine
 
+# Setup secret with certs on Vault server
 Setup instace and user certs on K8s cluster
 insert_secrets_on_k8s
 patch_vault_statefulset
 restart_pods
 
+# Setup Vault with database config, roles and policies
 config_vault_database
-
 create_roles
-
 create_policies
 
-# Results
+# Show results
 print_results
